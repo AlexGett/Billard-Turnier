dba84daf4f50a649573555e5d6f0902d0fe6fbfc
AlexGett <alex_gettinger@yahoo.com>
2025-07-26 23:04:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,47 @@
-const CACHE_NAME = 'billard-turnier-v2.6'; // Ändere die Version bei Änderungen an den gecachten Dateien!
-                                          // Habe die Version hier auf v1.3 erhöht, da sich die gecachten Dateien ändern.
+const CACHE_NAME = 'billard-tournament-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  // Füge hier die Pfade zu ALLEN deinen Icons hinzu, die du im 'icons/' Ordner hast
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
-  '/icons/LargeTile.scale-100.png', // Korrigierter Pfad für dein vorhandenes Icon
-  '/icons/android-launchericon-512-512.png', // Korrigierter Pfad für dein vorhandenes Icon
-  '/icons/1024.png' // Korrigierter Pfad für dein vorhandenes Icon
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './Logo.png', // Stellen Sie sicher, dass diese Datei existiert!
+  './Trophy.png', // Stellen Sie sicher, dass diese Datei existiert!
+  // Fügen Sie hier weitere Assets hinzu, die gecacht werden sollen (z.B. CSS, JS-Dateien)
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Service Worker: Cache geöffnet');
+      .then((cache) => {
+        console.log('Cache opened');
         return cache.addAll(urlsToCache);
       })
-      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Alten Cache löschen', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
         if (response) {
           return response;
         }
         return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
